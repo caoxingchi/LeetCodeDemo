22e9f4f9d4c2291f4c58799218e8be28e62a1965
itcolors <caoxingchi@outlook.com>
2021-06-07 10:57:53 +0800

    Initial commit

---
+++ src/test/java/site/xingchi/AppTest.java
@@ -0,0 +1,19 @@
+package site.xingchi;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("test");
+    }
+}
